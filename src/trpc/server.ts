177712c9dde488ca407e4f6eb50b44449d2244a4
Olimpikada <olimpikada@gmail.com>
2024-09-24 18:47:03 +0400

    a

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -9,6 +9,7 @@ import { createTRPCContext } from "~/server/api/trpc";
 import { createQueryClient } from "./query-client";
 import { auth } from "@clerk/nextjs/server";
 
+
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
